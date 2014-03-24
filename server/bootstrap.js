2c35f89d6ba8f20da26bd917583c6f5114a8cc87
Marcelo Schmidt <marcelo@marceloschmidt.com>
2014-03-23 23:43:13 -0300

    remove todos; add currentCount

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,19 +2,19 @@
 Meteor.startup(function () {
   if (Material.find().count() === 0) {
     var data = [
-      { name: "Guias de Eucalipto", quantity: { value: 80, unit: '' } },
-      { name: "Caibros", quantity: { value: 40, unit: '' } },
-      { name: "Pregos", quantity: { value: 12, unit: 'kg' } },
-      { name: "Tijolos", quantity: { value: 2500, unit: '' } },
-      { name: "Cimento", quantity: { value: 30, unit: 'sacos' } },
-      { name: "Ferro 8mm", quantity: { value: 15, unit: 'barras' } },
-      { name: "Ferro 5mm", quantity: { value: 12, unit: 'barras' } },
-      { name: "Areia", quantity: { value: 7, unit: 'm³' } },
-      { name: "Brita", quantity: { value: 4, unit: 'm³' } },
-      { name: "Telhas" },
-      { name: "Arame queimado" },
-      { name: "Alvenarite" },
-      { name: "Material elétrico" }
+      { name: "Guias de Eucalipto", quantity: { value: 80, unit: '' }, currentCount: 0 },
+      { name: "Caibros", quantity: { value: 40, unit: '' }, currentCount: 0 },
+      { name: "Pregos", quantity: { value: 12, unit: 'kg' }, currentCount: 0 },
+      { name: "Tijolos", quantity: { value: 2500, unit: '' }, currentCount: 0 },
+      { name: "Cimento", quantity: { value: 30, unit: 'sacos' }, currentCount: 0 },
+      { name: "Ferro 8mm", quantity: { value: 15, unit: 'barras' }, currentCount: 0 },
+      { name: "Ferro 5mm", quantity: { value: 12, unit: 'barras' }, currentCount: 0 },
+      { name: "Areia", quantity: { value: 7, unit: 'm³' }, currentCount: 0 },
+      { name: "Brita", quantity: { value: 4, unit: 'm³' }, currentCount: 0 },
+      { name: "Telhas", currentCount: 0 },
+      { name: "Arame queimado", currentCount: 0 },
+      { name: "Alvenarite", currentCount: 0 },
+      { name: "Material elétrico", currentCount: 0 }
     ];
 
     for (var i = 0; i < data.length; i++) {
